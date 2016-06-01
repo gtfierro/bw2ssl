0f5be6d4e5cea54c4d340e42f0eea5302735adef
Gabe Fierro <gtfierro225@gmail.com>
2016-06-01 00:24:17 -0700

    more playing around

--- bw2ssl.c
+++ bw2ssl.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <poll.h>
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -18,6 +20,8 @@ static int (*orig_bind)(int socket, const struct sockaddr *address,
             socklen_t address_len);
 static ssize_t (*orig_recvfrom)(int socket, void *buffer, size_t length, int flags,
             struct sockaddr *address, socklen_t *address_len);
+static ssize_t (*orig_recv)(int sockfd, void *buf, size_t len, int flags);
+static int (*orig_poll)(struct pollfd *fds, nfds_t nfds, int timeout);
 static int (*orig_listen)(int sockfd, int backlog);
 static ssize_t (*orig_read)(int fd, void *buf, size_t count);
 static ssize_t (*orig_write)(int fd, const void *buf, size_t count);
@@ -27,6 +31,7 @@ static int (*orig_connect)(int sockfd, const struct sockaddr *addr,
                    socklen_t addrlen);
 
 static int publish(int bw_sock, const char *uri, const void *data, size_t count);
+static int subscribe(int bw_sock, const char *uri, void *data, size_t count);
 
 // array of socket FDs that we emulate over bosswave
 static int socket_fds[1024];
@@ -51,8 +56,10 @@ static void init(void)
     orig_socket=dlsym(RTLD_NEXT, "socket");
     orig_bind=dlsym(RTLD_NEXT, "bind");
     orig_recvfrom=dlsym(RTLD_NEXT, "recvfrom");
+    orig_recv=dlsym(RTLD_NEXT, "recv");
     orig_listen=dlsym(RTLD_NEXT, "listen");
     orig_read=dlsym(RTLD_NEXT, "read");
+    orig_poll=dlsym(RTLD_NEXT, "poll");
     orig_write=dlsym(RTLD_NEXT, "write");
     orig_close=dlsym(RTLD_NEXT, "close");
     orig_accept=dlsym(RTLD_NEXT, "accept");
@@ -117,7 +124,7 @@ static void init(void)
     {
         printf("Unsuccessfully wrote entity to router\n");
     }
-    printf("%.*s",written,buf);
+    //printf("%.*s",written,buf);
 
     numread = 0;
     while (numread == 0)
@@ -168,8 +175,14 @@ int socket(int domain, int type, int protocol)
 int bind(int socket, const struct sockaddr *address, socklen_t address_len)
 {
     if(!initialized) init();
-    printf(">> called bind <<\n");
+    printf(">> called bind: socket %d<<\n", socket);
     printf("have fd? %d\n", have_fd(socket));
+    struct sockaddr_in *ip_src = (struct sockaddr_in*)address;
+    char *uri = malloc(128);
+    sprintf(uri, "%s/%s/%d", namespace, inet_ntoa(ip_src->sin_addr), ip_src->sin_port);
+    printf("uri for sub %s\n", uri);
+    socket_uris[socket] = uri;
+
     return orig_bind(socket, address, address_len);
 }
 
@@ -181,10 +194,28 @@ ssize_t recvfrom(int socket, void *buffer, size_t length, int flags,
     return orig_recvfrom(socket, buffer, length, flags, address, address_len);
 }
 
+ssize_t recv(int sockfd, void *buf, size_t len, int flags)
+{
+    if(!initialized) init();
+    printf(">> called recv: socket %d <<\n", sockfd);
+    if (have_fd(sockfd))
+    {
+        subscribe(bw_sock, socket_uris[sockfd], buf, len);
+    }
+    return orig_recv(sockfd, buf, len, flags);
+}
+
+int poll(struct pollfd *fds, nfds_t nfds, int timeout)
+{
+    if(!initialized) init();
+    printf(">> called poll <<\n");
+    return orig_poll(fds, nfds, timeout);
+}
+
 int listen(int sockfd, int backlog)
 {
     if(!initialized) init();
-    printf(">> called listen <<\n");
+    printf(">> called listen: socket %d <<\n", sockfd);
     return orig_listen(sockfd, backlog);
 }
 
@@ -192,6 +223,10 @@ ssize_t read(int fd, void *buf, size_t count)
 {
     if(!initialized) init();
     printf(">> called read <<\n");
+    if (have_fd(fd))
+    {
+        //subscribe(bw_sock, socket_uris[fd], buf, count);
+    }
     return orig_read(fd, buf, count);
 }
 
@@ -199,7 +234,10 @@ ssize_t write(int fd, const void *buf, size_t count)
 {
     if(!initialized) init();
     printf(">> called write <<\n");
-    publish(bw_sock, socket_uris[fd], buf, count);
+    if (have_fd(fd))
+    {
+        publish(bw_sock, socket_uris[fd], buf, count);
+    }
     return orig_write(fd, buf, count);
 }
 
@@ -213,8 +251,14 @@ int close(int fd)
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
 {
     if(!initialized) init();
-    printf(">> called accept <<\n");
-    return orig_accept(sockfd, addr, addrlen);
+    printf(">> called accept: socket %d<<\n", sockfd);
+    int incoming_fd = orig_accept(sockfd, addr, addrlen);
+    add_fd(incoming_fd);
+    struct sockaddr_in *ip_src = (struct sockaddr_in*)addr;
+    char uri[128];
+    sprintf(uri, "%s/%s/%d", namespace, inet_ntoa(ip_src->sin_addr), ip_src->sin_port);
+    printf("uri for sub %s\n", uri);
+    return incoming_fd;
 }
 
 int connect (int sockfd, const struct sockaddr *addr, socklen_t addrlen)
@@ -246,13 +290,31 @@ static int publish(int bw_sock, const char *uri, const void *data, size_t count)
     printf("written: %i\n", written);
     if (orig_write(bw_sock, buf, written) != written)
     {
-        printf("Unsuccessfully wrote entity to router\n");
+        printf("Unsuccessfully wrote publish to router\n");
     }
-    printf("%.*s",written,buf);
+    //printf("%.*s",written,buf);
     return written;
-    //printf "publ %010d %010d\n" 0 2 >&3
-    //echo -e "kv uri 14\n410.dev/foobar" >&3
-    //echo -e "kv autochain 4\ntrue" >&3
-    //echo -e "po 64.0.1.0: 5\nhello" >&3
-    //echo -e "end" >&3
+}
+
+//ssize_t read(int fd, void *buf, size_t count)
+static int subscribe(int bw_sock, const char *uri, void *data, size_t count)
+{
+    char buf[512+count];
+    int written = 0;
+    written += sprintf(buf, "subs %010d %010d\n", 0, 2);
+    written += sprintf(buf+written, "kv uri %zd\n%s\n", strlen(uri), uri);
+    written += sprintf(buf+written, "kv autochain 4\ntrue\n");
+    written += sprintf(buf+written, "kv unpack 4\ntrue\n");
+    written += sprintf(buf+written, "\nend\n");
+    if (orig_write(bw_sock, buf, written) != written)
+    {
+        printf("Unsuccessfully wrote subscribe to router\n");
+    }
+
+    int numread = 0;
+    while (numread == 0)
+    {
+        numread = orig_read(bw_sock, buf, 1024);
+    }
+    printf("from bw sock>>>%.*s",numread,buf);
 }
